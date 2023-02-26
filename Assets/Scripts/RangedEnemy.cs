b04d6202538b874491d2f374b0f0529136e82650
Mariha-B <111390252+Mariha-B@users.noreply.github.com>
2023-02-26 16:13:02 +0000

    Ranged script

--- Assets/Scripts/RangedEnemy.cs
+++ Assets/Scripts/RangedEnemy.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class RangedEnemy : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+    
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+
+    //REferences
+    
+
+
+
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
